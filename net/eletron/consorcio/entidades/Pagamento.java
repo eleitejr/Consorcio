03fbdf7f1ef1561ef62c72b9f9ec6d382914535d
Erasmo de Castro Leite Junior <eleitejr@gmail.com>
2015-05-07 15:50:09 -0300

    Atualização 07/05/15 15:44

--- net/eletron/consorcio/entidades/Pagamento.java
+++ net/eletron/consorcio/entidades/Pagamento.java
@@ -1,5 +1,10 @@
 package net.eletron.consorcio.entidades;
 
 public class Pagamento {
+  private int id;
+  private String banco;
+  private double valor_facial;
+  private Date data_vcto;
+  private Date data_pgto;
 
 }
